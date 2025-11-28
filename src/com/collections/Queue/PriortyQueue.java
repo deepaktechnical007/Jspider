e5fe72fa0682c1b282179b1fb6d024af5fed3b61
DPK <Todeepakbabu@gmail.com>
2025-11-28 11:34:52 +0530

    dpk

--- src/com/collections/Queue/PriortyQueue.java
+++ src/com/collections/Queue/PriortyQueue.java
@@ -1,30 +1,29 @@
-//package com.collections.Queue;
-//
-//
-//public class PriortyQueue<S> {
-//
-//    public static void main(String[] args) {
-//
-//        PriortyQueue<String> pq1;
-//        pq1 = new PriortyQueue<>();
-//
-//        pq1.add("A");
-//        pq1.add("C");
-//        pq1.add("B");
-//        pq1.add("D");
-//        pq1.add("C");
-//
-//        System.out.println("Size : "+ pq1.size());
-//        System.out.println("isEmpty : "+pq1.isEmpty());
-//
-//
-//        while (pq1.isEmpty()!=true)
-//        {
-//            System.out.println(pq1.poll());
-//        }
-//
-//        System.out.println("size : "+pq1.size());
-//        System.out.println("isEmpty : "+ pq1.isEmpty());
-//    }
-//
-//}
+package com.collections.Queue;
+
+
+public class PriortyQueue {
+
+    public static void main(String[] args) {
+
+        PriortyQueue<String> pq1 = new PriortyQueue<>();
+
+        pq1.add("A");
+        pq1.add("C");
+        pq1.add("B");
+        pq1.add("D");
+        pq1.add("C");
+
+        System.out.println("Size : "+ pq1.size());
+        System.out.println("isEmpty : "+pq1.isEmpty());
+
+
+        while (pq1.isEmpty()!=true)
+        {
+            System.out.println(pq1.poll());
+        }
+
+        System.out.println("size : "+pq1.size());
+        System.out.println("isEmpty : "+ pq1.isEmpty());
+    }
+
+}
